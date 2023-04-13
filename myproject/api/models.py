d964e8d1162cc3a86327e075e07b15f6244b2d09
digregorio <asantana@trt4.jus.br>
2023-04-13 01:12:50 -0300

    adiciona docstrings

--- myproject/api/models.py
+++ myproject/api/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
 class PessoaProfissional(models.Model):
+    """
+    Representa uma pessoa profissional.
+
+    Atributos:
+        nome (str): Nome da pessoa profissional.
+        nome_social (str): Nome social da pessoa profissional (opcional).
+    """
     nome = models.CharField(max_length=255)
     nome_social = models.CharField(max_length=255, blank=True, null=True)
 
@@ -8,6 +15,13 @@ class PessoaProfissional(models.Model):
         return self.nome
 
 class Consulta(models.Model):
+    """
+    Representa uma consulta.
+
+    Atributos:
+        pessoa_profissional (PessoaProfissional): Pessoa profissional que realizou a consulta.
+        data (datetime): Data em que a consulta foi realizada.
+    """
     pessoa_profissional = models.ForeignKey(PessoaProfissional, on_delete=models.CASCADE)
     data = models.DateTimeField()
 
--- myproject/api/serializers.py
+++ myproject/api/serializers.py
@@ -2,11 +2,17 @@ from rest_framework import serializers
 from .models import PessoaProfissional, Consulta
 
 class PessoaProfissionalSerializer(serializers.ModelSerializer):
+    """
+    Serializer para o modelo PessoaProfissional.
+    """
     class Meta:
         model = PessoaProfissional
         fields = ['id', 'nome', 'nome_social']
 
 class ConsultaSerializer(serializers.ModelSerializer):
+    """
+    Serializer para o modelo Consulta.
+    """
     class Meta:
         model = Consulta
         fields = ['id', 'pessoa_profissional', 'data']
